e69bfee6c714c476b3d9d4116aea694ca577de1e
Carl Georg Biermann <carlgeorg@gmx.net>
2021-06-01 12:42:35 +0200

    Error analysis tutorial: modify series generation and expand tests

--- testsuite/scripts/tutorials/test_error_analysis_part1.py
+++ testsuite/scripts/tutorials/test_error_analysis_part1.py
@@ -17,6 +17,8 @@
 
 import unittest as ut
 import importlib_wrapper
+import numpy as np
+import scipy.signal
 
 tutorial, skipIfMissingFeatures = importlib_wrapper.configure_and_import(
     filepath="@TUTORIALS_DIR@/error_analysis/error_analysis_part1.py",
@@ -27,9 +29,50 @@ tutorial, skipIfMissingFeatures = importlib_wrapper.configure_and_import(
 @skipIfMissingFeatures
 class Tutorial(ut.TestCase):
 
+    def ar_1_process(self, n, c, phi, eps):
+        y0 = np.random.normal(loc=c / (1 - phi),
+                              scale=np.sqrt(eps**2 / (1 - phi**2)))
+        y = c + np.random.normal(loc=0.0, scale=eps, size=n - 1)
+        y = np.insert(y, 0, y0)
+        # get an AR(1) process from an ARMA(p,q) process with p=1 and q=0
+        y = scipy.signal.lfilter([1.], [1., -phi], y)
+        return y
+
+    def test_ar1_implementation(self):
+        with self.assertRaises(ValueError):
+            tutorial.ar_1_process(10, 1.0, 1.1, 3.0)
+        with self.assertRaises(ValueError):
+            tutorial.ar_1_process(10, 1.0, -1.1, 3.0)
+
+        for seed in range(5):
+            for eps in [0.5, 1., 2.]:
+                for phi in [0.1, 0.8, 0.999, -0.3]:
+                    c = eps / 2.
+                    np.random.seed(seed)
+                    seq = tutorial.ar_1_process(10, c, phi, eps)
+                    np.random.seed(seed)
+                    ref = self.ar_1_process(10, c, phi, eps)
+                    np.testing.assert_allclose(seq, ref, atol=1e-12, rtol=0)
+
     def test(self):
-        self.assertAlmostEqual(tutorial.fit_params[2], 0.024, delta=0.002)
-        self.assertAlmostEqual(tutorial.avg, 13.35, delta=0.1)
+        self.assertLess(abs(tutorial.PHI_1), 1.0)
+        self.assertLess(abs(tutorial.PHI_2), 1.0)
+
+        # The analytic expressions for the AR(1) process are taken from
+        # https://en.wikipedia.org/wiki/Autoregressive_model#Example:_An_AR(1)_process
+        # (accessed June 2021)
+        MU_1 = tutorial.C_1 / (1 - tutorial.PHI_1)
+        SIGMA_1 = np.sqrt(tutorial.EPS_1 ** 2 / (1 - tutorial.PHI_1 ** 2))
+        TAU_EXP_1 = -1 / np.log(tutorial.PHI_1)
+        # The autocorrelation is exponential, thus tau_exp = tau_int, and
+        # therefore
+        SEM_1 = np.sqrt(2 * SIGMA_1 ** 2 * TAU_EXP_1 / tutorial.N_SAMPLES)
+
+        self.assertAlmostEqual(
+            tutorial.fit_params[2],
+            SEM_1,
+            delta=0.1 * SEM_1)
+        self.assertAlmostEqual(tutorial.avg, MU_1, delta=SEM_1 * 2.0)
 
 
 if __name__ == "__main__":
